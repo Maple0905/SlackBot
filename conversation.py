211266caab65f1b9d7a5ed52cee5911683c40ca5
Maple0905 <foreverNo1Techi@gmail.com>
2023-10-02 09:15:49 -0400

    working on thread

--- conversation.py
+++ conversation.py
@@ -337,51 +337,50 @@ def getThreadMessageHistory(source_token, target_token, source_channel_id, targe
             DB_CONN.commit()
             if response[0][4] is not None and latest_thread_ts > response[0][4]  :
                 rePostThreads(source_token, target_token, source_channel_id, target_channel_id, thread_messages, response[0][4])
-
-                if target_token == TARGET_BOT_TOKEN :
-                    target_user_client = WebClient(token=TARGET_USER_TOKEN)
-                if target_token == SOURCE_BOT_TOKEN :
-                    target_user_client = WebClient(token=SOURCE_USER_TOKEN)
-                #Edit Thread Message
-                # new_thread_messages = []
-                # for message in thread_messages :
-                #     if message['latest_reply'] > response[0][4] :
-                #         new_thread_messages.append(message)
-                # print('new : ', new_thread_messages)
-                for thread_message in thread_messages :
-                    thread_response = source_client.conversations_replies(
-                        channel=source_channel_id,
-                        ts=thread_message['thread_ts'],
-                    )
-                    repost_thread_messages = thread_response['messages']
-                    print('new thread message content : ', repost_thread_messages)
-                    DB_CURSOR.execute("SELECT * FROM conversation WHERE source_ts = %s", thread_message['thread_ts'])
-                    response = DB_CURSOR.fetchall()
-                    if (len(response)) == 0 :
-                        continue
-                    else :
-                        repost_ts = response[0][4]
-                        for repost_thread_message in repost_thread_messages :
-                            if 'parent_user_id' in repost_thread_message and 'edited' in repost_thread_message :
-                                print('new thread message : ', repost_thread_message)
-                                user_response = source_client.users_profile_get(user=repost_thread_message['user'])
-                                display_name = user_response["profile"]["real_name"]
-                                query = "SELECT * FROM thread_conversation WHERE source_message_ts = %s AND source_channel_id = %s AND source_thread_ts = %s"
-                                DB_CURSOR.execute(query, (thread_message['thread_ts'], source_channel_id, repost_thread_message['ts']))
-                                response = DB_CURSOR.fetchall()
-                                if len(response) != 0 :
-                                    display_text = '*@' + display_name + '* mentioned. :mega:'
-                                    if 'files' in message :
-                                        text = display_text + '\n' + message['text']
-                                    else :
-                                        text = message['text']
-                                    print(target_channel_id, ' ', response[0][6], ' ', text)
-                                    target_user_client.chat_update(
-                                        channel=target_channel_id,
-                                        ts=response[0][6],
-                                        text=text
-                                    )
-                DB_CONN.commit()
+            if target_token == TARGET_BOT_TOKEN :
+                target_user_client = WebClient(token=TARGET_USER_TOKEN)
+            if target_token == SOURCE_BOT_TOKEN :
+                target_user_client = WebClient(token=SOURCE_USER_TOKEN)
+            #Edit Thread Message
+            # new_thread_messages = []
+            # for message in thread_messages :
+            #     if message['latest_reply'] > response[0][4] :
+            #         new_thread_messages.append(message)
+            # print('new : ', new_thread_messages)
+            for thread_message in thread_messages :
+                thread_response = source_client.conversations_replies(
+                    channel=source_channel_id,
+                    ts=thread_message['thread_ts'],
+                )
+                repost_thread_messages = thread_response['messages']
+                print('new thread message content : ', repost_thread_messages)
+                DB_CURSOR.execute("SELECT * FROM conversation WHERE source_ts = %s", thread_message['thread_ts'])
+                response = DB_CURSOR.fetchall()
+                if (len(response)) == 0 :
+                    continue
+                else :
+                    repost_ts = response[0][4]
+                    for repost_thread_message in repost_thread_messages :
+                        if 'parent_user_id' in repost_thread_message and 'edited' in repost_thread_message :
+                            print('new thread message : ', repost_thread_message)
+                            user_response = source_client.users_profile_get(user=repost_thread_message['user'])
+                            display_name = user_response["profile"]["real_name"]
+                            query = "SELECT * FROM thread_conversation WHERE source_message_ts = %s AND source_channel_id = %s AND source_thread_ts = %s"
+                            DB_CURSOR.execute(query, (thread_message['thread_ts'], source_channel_id, repost_thread_message['ts']))
+                            response = DB_CURSOR.fetchall()
+                            if len(response) != 0 :
+                                display_text = '*@' + display_name + '* mentioned. :mega:'
+                                if 'files' in message :
+                                    text = display_text + '\n' + message['text']
+                                else :
+                                    text = message['text']
+                                print(target_channel_id, ' ', response[0][6], ' ', text)
+                                target_user_client.chat_update(
+                                    channel=target_channel_id,
+                                    ts=response[0][6],
+                                    text=text
+                                )
+            DB_CONN.commit()
 
     except SlackApiError as e:
         print(f"Error posting message: {e.response['error']}")
